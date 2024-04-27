d8cde051629c616b5990b2fd352c04337dc79b60
Omer Khan <notomerkhan@gmail.com>
2024-04-27 11:29:32 -0400

    Initial Commit

---
+++ LiDar/LiDarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiDarApp.swift
+//  LiDar
+//
+//  Created by Omer Khan on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LiDarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
